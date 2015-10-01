84569a0bbf1a15dec904b89022a65f18b47d99e8
Chris Kirby <chris@infinitusdev.com>
2015-10-01 16:01:56 -0500

    move to release version, updated readme, and changed badges to appveyor
    fixes #11

--- src/Raygun.Diagnostics.net35/Properties/AssemblyInfo.cs
+++ src/Raygun.Diagnostics.net35/Properties/AssemblyInfo.cs
@@ -34,4 +34,4 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("0.7.5")]
 [assembly: AssemblyFileVersion("0.7.5")]
-[assembly: AssemblyInformationalVersion("0.7.5-b2")]
+[assembly: AssemblyInformationalVersion("0.7.5")]
